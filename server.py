dfc5963d9b42d39baa8f83acfd1c673dad6b97fa
WeiyaoLi <nolanwyl@gmail.com>
2023-03-30 01:59:26 -0400

    search in progress

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 """
 Columbia's COMS W4111.001 Introduction to Databases
 Example Webserver
@@ -12,11 +11,10 @@ import os
 # accessible as a variable in index.html:
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response, abort
+from flask import Flask, request, render_template, g, redirect, Response, abort, jsonify
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
 
 #
 # The following is a dummy URI that does not connect to a valid database.
@@ -33,9 +31,8 @@ app = Flask(__name__, template_folder=tmpl_dir)
 # Modify these with your own credentials you received from TA!
 DATABASE_USERNAME = "yz4429"
 DATABASE_PASSWRD = "3372"
-DATABASE_HOST = "34.148.107.47" # change to 34.28.53.86 if you used database 2 for part 2
+DATABASE_HOST = "34.148.107.47"  # change to 34.28.53.86 if you used database 2 for part 2
 DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/project1"
-
 
 #
 # This line creates a database engine that knows how to connect to the URI above.
@@ -47,45 +44,47 @@ engine = create_engine(DATABASEURI, future=True)
 # Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
 #
 with engine.connect() as conn:
-	create_table_command = """
+    create_table_command = """
 	CREATE TABLE IF NOT EXISTS test (
 		id serial,
 		name text
 	)
 	"""
-	res = conn.execute(text(create_table_command))
-	insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
-	res = conn.execute(text(insert_table_command))
-	# you need to commit for create, insert, update queries to reflect
-	conn.commit()
+    res = conn.execute(text(create_table_command))
+    insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
+    res = conn.execute(text(insert_table_command))
+    # you need to commit for create, insert, update queries to reflect
+    conn.commit()
 
 
 @app.before_request
 def before_request():
-	"""
+    """
 	This function is run at the beginning of every web request 
 	(every time you enter an address in the web browser).
 	We use it to setup a database connection that can be used throughout the request.
 
 	The variable g is globally accessible.
 	"""
-	try:
-		g.conn = engine.connect()
-	except:
-		print("uh oh, problem connecting to database")
-		import traceback; traceback.print_exc()
-		g.conn = None
+    try:
+        g.conn = engine.connect()
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback;
+        traceback.print_exc()
+        g.conn = None
+
 
 @app.teardown_request
 def teardown_request(exception):
-	"""
+    """
 	At the end of the web request, this makes sure to close the database connection.
 	If you don't, the database could run out of memory!
 	"""
-	try:
-		g.conn.close()
-	except Exception as e:
-		pass
+    try:
+        g.conn.close()
+    except Exception as e:
+        pass
 
 
 #
@@ -103,7 +102,7 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
-	"""
+    """
 	request is a special object that Flask provides to access web request information:
 
 	request.method:   "GET" or "POST"
@@ -113,54 +112,53 @@ def index():
 	See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
 	"""
 
-	# DEBUG: this is debugging code to see what request looks like
-	print(request.args)
+    # DEBUG: this is debugging code to see what request looks like
+    print(request.args)
 
+    #
+    # example of a database query
+    #
+    select_query = "SELECT name from test"
+    cursor = g.conn.execute(text(select_query))
+    names = []
+    for result in cursor:
+        names.append(result[0])
+    cursor.close()
 
-	#
-	# example of a database query
-	#
-	select_query = "SELECT name from test"
-	cursor = g.conn.execute(text(select_query))
-	names = []
-	for result in cursor:
-		names.append(result[0])
-	cursor.close()
+    #
+    # Flask uses Jinja templates, which is an extension to HTML where you can
+    # pass data to a template and dynamically generate HTML based on the data
+    # (you can think of it as simple PHP)
+    # documentation: https://realpython.com/primer-on-jinja-templating/
+    #
+    # You can see an example template in templates/index.html
+    #
+    # context are the variables that are passed to the template.
+    # for example, "data" key in the context variable defined below will be
+    # accessible as a variable in index.html:
+    #
+    #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+    #     <div>{{data}}</div>
+    #
+    #     # creates a <div> tag for each element in data
+    #     # will print:
+    #     #
+    #     #   <div>grace hopper</div>
+    #     #   <div>alan turing</div>
+    #     #   <div>ada lovelace</div>
+    #     #
+    #     {% for n in data %}
+    #     <div>{{n}}</div>
+    #     {% endfor %}
+    #
+    context = dict(data=names)
 
-	#
-	# Flask uses Jinja templates, which is an extension to HTML where you can
-	# pass data to a template and dynamically generate HTML based on the data
-	# (you can think of it as simple PHP)
-	# documentation: https://realpython.com/primer-on-jinja-templating/
-	#
-	# You can see an example template in templates/index.html
-	#
-	# context are the variables that are passed to the template.
-	# for example, "data" key in the context variable defined below will be 
-	# accessible as a variable in index.html:
-	#
-	#     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-	#     <div>{{data}}</div>
-	#     
-	#     # creates a <div> tag for each element in data
-	#     # will print: 
-	#     #
-	#     #   <div>grace hopper</div>
-	#     #   <div>alan turing</div>
-	#     #   <div>ada lovelace</div>
-	#     #
-	#     {% for n in data %}
-	#     <div>{{n}}</div>
-	#     {% endfor %}
-	#
-	context = dict(data = names)
+    #
+    # render_template looks in the templates/ folder for files.
+    # for example, the below file reads template/index.html
+    #
+    return render_template("index.html", **context)
 
-
-	#
-	# render_template looks in the templates/ folder for files.
-	# for example, the below file reads template/index.html
-	#
-	return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at:
@@ -172,52 +170,82 @@ def index():
 #
 @app.route('/another')
 def another():
-	return render_template("another.html")
+    return render_template("another.html")
 
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
-	# accessing form inputs from user
-	name = request.form['name']
-	
-	# passing params in for each variable into query
-	params = {}
-	params["new_name"] = name
-	g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
-	g.conn.commit()
-	return redirect('/')
+    # accessing form inputs from user
+    name = request.form['name']
+
+    # passing params in for each variable into query
+    params = {}
+    params["new_name"] = name
+    g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
+    g.conn.commit()
+    return redirect('/')
 
 
 @app.route('/login')
 def login():
-	abort(401)
-	# this_is_never_executed()
+    abort(401)
+
+
+# this_is_never_executed()
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form.get('query')
+    select_query = "SELECT title, year, name, description " \
+                   "FROM artwork " \
+                   "JOIN artist a ON artwork.artist_id = a.artist_id"
+
+    dictionary = {'title': 'title', 'artist': 'name'}
+    filters = list(filter(lambda x: request.form.get(x) is not None, ['title', 'artist']))
+
+    for i in range(len(filters)):
+        current_filter = filters[i]
+        if i == 0:
+            select_query += ' WHERE '
+        select_query += dictionary[current_filter] + ' LIKE \'%' + query + '%\''
+        if i < len(filters) - 1:
+            select_query += ' OR '
+    print(select_query)
+    cursor = g.conn.execute(text(select_query))
+    results = []
+    for result in cursor:
+        temp = []
+        for data in result:
+            temp.append(data)
+        results.append(temp)
+    cursor.close()
+    # return jsonify({'result': titles})
+    return render_template('artworks.html', results=results, heads=['Title', 'Year', 'Artist', 'Description'], query=query)
 
 
 if __name__ == "__main__":
-	import click
+    import click
 
-	@click.command()
-	@click.option('--debug', is_flag=True)
-	@click.option('--threaded', is_flag=True)
-	@click.argument('HOST', default='0.0.0.0')
-	@click.argument('PORT', default=8111, type=int)
-	def run(debug, threaded, host, port):
-		"""
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
 		This function handles command line parameters.
 		Run the server using:
-
 			python server.py
-
 		Show the help text using:
-
 			python server.py --help
-
 		"""
 
-		HOST, PORT = host, port
-		print("running on %s:%d" % (HOST, PORT))
-		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
-	run()
+
+    run()
